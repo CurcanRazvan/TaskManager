41d3368fc6942fe20af81e324bc2c44b33c19799
Curcan Razvan <curcanrazvandenis@gmail.com>
2024-12-15 08:54:17 +0200

    Initial commit.

---
+++ src/main/java/ro/digitalnation/models/Task.java
@@ -0,0 +1,11 @@
+package ro.digitalnation.models;
+
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+
+
+public class Task {
+
+}
